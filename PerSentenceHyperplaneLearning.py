cfb77c33f39380bd3a3b638dafb1a8b4109c3d85
Rahul Madhavan <rahul.maddy@gmail.com>
2022-11-22 17:48:56 +0530

    going on and on

--- PerSentenceHyperplaneLearning.py
+++ PerSentenceHyperplaneLearning.py
@@ -1,9 +1,10 @@
-import time, numpy as np, pandas as pd, torch
+import time, numpy as np, pandas as pd, torch, random
 from wordSentimentStatsInDataset import getCleanedWordsFromDataset, computeLogRatios
 from tokenizers.normalizers import NFD, StripAccents, Strip, Lowercase, BertNormalizer
 from wordUtils import getSpacyVector, getTokenTransformerEmbedding, getListOfWordsFromSentences
 from transformers import DistilBertTokenizerFast, DistilBertModel
 from tqdm import tqdm
+from collections import Counter
 
 
 def getClassFromLogRatio(value, threshold=0.5):
@@ -22,12 +23,12 @@ def getMetrics(confusionMatrix):
     return accuracy, truePositive, trueNegative, falsePositive, falseNegative
 
 
-def getBertEmbeddings(words,device):
+def getBertEmbeddings(words, device):
     tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-uncased")
     model = DistilBertModel.from_pretrained("distilbert-base-uncased")
     model.to(device)
     model.eval()
-    wordsVecDict = {word: getTokenTransformerEmbedding(word, model, tokenizer,device)[0][1] for word in words}
+    wordsVecDict = {word: getTokenTransformerEmbedding(word, model, tokenizer, device)[0][1] for word in words}
     # print("wordsVecDict[the]",wordsVecDict["the"])
     return wordsVecDict
 
@@ -35,7 +36,6 @@ def getBertEmbeddings(words,device):
 def getSpacyEmbeddings(words):
     wordsVecDict = {word: getSpacyVector(word) for word in words}
     return wordsVecDict
-
 
 
 def printMetrics(embeddingName, modelName, accuracy, truePositive, trueNegative, falsePositive, falseNegative):
@@ -74,8 +74,7 @@ def getAvgZW(sentences, w, b, device):
 
 
 def getWordVecScore(wordVec, w, b):
-    return torch.matmul(wordVec, w) + b
-
+    return wordVec @ w + b
 
 
 def computeAugmentedWordsDict(wordsDict, w, b):
@@ -86,31 +85,33 @@ def computeAugmentedWordsDict(wordsDict, w, b):
     return outDict
 
 
-def getSumNumWZFromWordsDict(wordsDict):
-    avgW, avgZ = 0.0, 0.0
+def getSumNumWZFromWordsDict(wordsDict, device="cpu"):
+    avgW, avgZ = torch.zeros(1, requires_grad=True, device=device), torch.zeros(1, requires_grad=True, device=device)
     numW, numZ = 0, 0
     for word, (vec, score, inZ) in tqdm(wordsDict.items()):
         if inZ:
-            avgZ += vec
+            avgZ = avgZ + vec
             numZ += 1
         else:
-            avgW += vec
+            avgW = avgW + vec
             numW += 1
     avgW = avgW / numW if numW != 0 else 0.0
     avgZ = avgZ / numZ if numZ != 0 else 0.0
     return avgW, avgZ
 
 
-def computeAvgLossFromWordsDict(posWordsDict, negWordsDict):
-    avgWPos, avgZPos = getSumNumWZFromWordsDict(posWordsDict)
-    avgWNeg, avgZNeg = getSumNumWZFromWordsDict(negWordsDict)
+def computeAvgLossFromWordsDict(posWordsDict, negWordsDict, device):
+    avgWPos, avgZPos = getSumNumWZFromWordsDict(posWordsDict, device)
+    avgWNeg, avgZNeg = getSumNumWZFromWordsDict(negWordsDict, device)
     return avgWPos, avgZPos, avgWNeg, avgZNeg
 
 
 if __name__ == "__main__":
     startTime = time.time()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("device=",device)
+    torch.manual_seed(8)
+    random.seed(8)
+    print("device=", device)
 
     filename = "datasets/combDev.csv"
     # filename = "datasets/combTrain.csv"
@@ -129,18 +130,76 @@ if __name__ == "__main__":
     print("got list of words")
 
     # Get bert embeddings
-    posVecs = getBertEmbeddings(posWords,device)
-    negVecs = getBertEmbeddings(negWords,device)
+    posVecs = getBertEmbeddings(posWords, device)
+    negVecs = getBertEmbeddings(negWords, device)
     allVecs = posVecs | negVecs
     print("got BERT embeddings")
     # Randomly initialize Hyperplane to 768D for size of bert embeddings
-    Hyp_w = torch.randn(768, 1, requires_grad=True, device=device)  # 300 rows and 1 columns for spacy
-    Hyp_b = torch.randn(1, requires_grad=True, device=device)
+    Hyp_w = torch.randn(768, 1, requires_grad=True, device=device)  # 768 rows and 1 columns for spacy
+    Hyp_b = torch.randn(1, 1, requires_grad=True, device=device)
+
+    posWordsCount = Counter(posWords)
+    negWordsCount = Counter(negWords)
+
+    print("created counters")
+
+    for words, wordCounts in posWordsCount.items():
+        posVecs[words] = posVecs[words] * wordCounts
+
+    posVecTuples = tuple(posVecs.values())
+    posVecMatrix = torch.stack(posVecTuples, 0)
+
+    for words, wordCounts in negWordsCount.items():
+        negVecs[words] = negVecs[words] * wordCounts
+
+    negVecTuples = tuple(negVecs.values())
+    negVecMatrix = torch.stack(negVecTuples, 0)
+    print("posVecMatrix.shape", posVecMatrix.shape)
+    print("negVecMatrix.shape", negVecMatrix.shape)
+
+    posValMatrix = torch.matmul(posVecMatrix, Hyp_w)
+    posValMatrix = posValMatrix + torch.matmul(torch.ones(posValMatrix.shape[0], 1, device=device), Hyp_b)
+
+    negValMatrix = torch.matmul(negVecMatrix, Hyp_w)
+    negValMatrix = negValMatrix + torch.matmul(torch.ones(negValMatrix.shape[0], 1, device=device), Hyp_b)
+
+    posAvgZVecs = torch.sign(torch.nn.functional.relu(posValMatrix))
+    posAvgWVecs = torch.sign(torch.nn.functional.relu(-posValMatrix))
+    negAvgZVecs = torch.sign(torch.nn.functional.relu(negValMatrix))
+    negAvgWVecs = torch.sign(torch.nn.functional.relu(-negValMatrix))
+
+    posAvgZVecs = posAvgZVecs@posValMatrix
+    posAvgWVecs = posAvgWVecs @ posValMatrix
+
+    timeTaken = time.time() - startTime
+    print("Time Taken:", timeTaken)
+
+    exit()
+
+
+
+    posZMatrix = (posValMatrix >= 0) * 1
+    posWMatrix = (posValMatrix < 0) * 1
+
+    posnumZs = sum(posZMatrix)[0]
+    posnumWs = sum(posWMatrix)[0]
+
+    negZMatrix = (negValMatrix >= 0) * 1
+    negWMatrix = (negValMatrix < 0) * 1
+
+    negnumZs = sum(negZMatrix)[0]
+    negnumWs = sum(negWMatrix)[0]
+
+    # posAvgZVecs = torch.sign(torch.nn.functional.relu(posValMatrix))
+    # posAvgWVecs = torch.sign(torch.nn.functional.relu(-posValMatrix))
+    # negAvgZVecs = torch.sign(torch.nn.functional.relu(negValMatrix))
+    # negAvgWVecs = torch.sign(torch.nn.functional.relu(-negValMatrix))
+
 
     posWordsDict = computeAugmentedWordsDict(posVecs, Hyp_w, Hyp_b)
     negWordsDict = computeAugmentedWordsDict(negVecs, Hyp_w, Hyp_b)
-    avgWPos, avgZPos, avgWNeg, avgZNeg = computeAvgLossFromWordsDict(posWordsDict, negWordsDict)
-
+    avgWPos, avgZPos, avgWNeg, avgZNeg = computeAvgLossFromWordsDict(posWordsDict, negWordsDict, device)
+    print("computed avgWZPosNeg scores")
     # yValsDict = {key: getClassFromLogRatio(val, threshold=0.5) for key, val in sortedAbsLogRatiosOfWords.items()}
 
     # # True Values
@@ -159,9 +218,7 @@ if __name__ == "__main__":
 
     print("lossScore=", lossScore)
     lossScore.backward()
-    print("Hyp_w.grad=",Hyp_w.grad)
+    print("Hyp_w.grad=", Hyp_w.grad)
     print("Hyp_b.grad=", Hyp_b.grad)
 
 
-    timeTaken = time.time() - startTime
-    print("Time Taken:", timeTaken)
